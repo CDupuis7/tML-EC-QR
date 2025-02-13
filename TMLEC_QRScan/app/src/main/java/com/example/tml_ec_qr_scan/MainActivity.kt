cc31fce267dea51a9fc7e05ad549f584b755dc35
CDupuis7 <cedupuis2002@gmail.com>
2025-02-12 22:15:27 -0600

    Added Project

---
+++ TMLEC_QRScan/app/src/main/java/com/example/tml_ec_qr_scan/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.tml_ec_qr_scan
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tml_ec_qr_scan.ui.theme.TMLEC_QRScanTheme
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TMLEC_QRScanTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Text(
+                                    text = "tML-EC QR Scan Project", // Title
+                                    textAlign = TextAlign.Center, // Center
+                                    style = MaterialTheme.typography.titleLarge
+                                )
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    // Main Screen Content
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ){
+                        // Button
+                        Button(
+                            onClick = {},
+                            modifier = Modifier
+                                .align(Alignment.BottomCenter)
+                                .padding(16.dp)
+
+                        ){
+                            Text(text = "Scan")
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+//Compose Examples:
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    TMLEC_QRScanTheme {
+//        Greeting("Android")
+//    }
+//}
